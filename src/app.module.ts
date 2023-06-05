c18e3f1bc3efbcacc24be415d3d5173eb22d0942
yuichiro miyamae <yu1ro.mya@gmail.com>
2023-06-06 01:01:50 +0900

    add: graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
+import { AppResolver } from './app.resolver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<MercuriusDriverConfig>({
+      driver: MercuriusDriver,
+      graphiql: true,
+      autoSchemaFile: true,
+    }),
+  ],
+  providers: [AppResolver],
 })
 export class AppModule {}
